62dd1869ebd8f381120c75dc2bb358489132c832
rosmat <rosmat@o2.pl>
2016-02-27 14:26:36 +0100

    poprawka do testu plaintext

--- src/main/java/com/mycompany/helloworld/web/PlaintextController.java
+++ src/main/java/com/mycompany/helloworld/web/PlaintextController.java
@@ -2,8 +2,9 @@ package com.mycompany.helloworld.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 final class PlaintextController {
 
     private static final String HELLO_WORLD = "Hello, World!";
